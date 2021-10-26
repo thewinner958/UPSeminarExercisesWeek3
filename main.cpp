c78bf7fef7af14687c7189126b1d052508b08bd6
TheWinner <33218314+thewinner958@users.noreply.github.com>
2021-10-26 20:23:15 +0300

    done with exercise 0 & 1

--- main.cpp
+++ main.cpp
@@ -2,6 +2,25 @@
 
 using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Exercise 0
+    int number;
+    cin >> number;
+    (number >= 5 && number <= 20) && number % 2 == 0 ? cout << "even\n" : cout << "odd\n";
+
+    // Exercise 1
+    int n;
+    cin >> n;
+    int dgt1 = n / 100;
+    int dgt2 = (n / 100) % 10;
+    int dgt3 = n % 10;
+    int dgtMultiplication = dgt1 * dgt2 * dgt3;
+    int dgtSum = dgt1 + dgt2 + dgt3;
+    if (dgtMultiplication >= 100) {
+        int dgtDifference = dgtMultiplication - dgtSum;
+        cout << "YES " << dgtDifference << endl;
+    }
+    else {
+        cout << "NO " << dgtMultiplication << endl;
+    }
     return 0;
 }
